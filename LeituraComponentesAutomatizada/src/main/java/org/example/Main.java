36a02f3818db160e6bfd626fba5ccfbec9538d89
Davi-Hilario <davihilario23@gmail.com>
2023-11-13 19:00:01 -0300

    nova classe ComponenteMedida e ComponenteMedidaComando

--- LeituraComponentesAutomatizada/src/main/java/org/example/Main.java
+++ LeituraComponentesAutomatizada/src/main/java/org/example/Main.java
@@ -1,15 +1,9 @@
 package org.example;
 
-import com.github.britooo.looca.api.group.rede.Rede;
-import org.example.business.ComponenteMedida;
+import org.example.business.ComponenteMedidaComando;
 import org.example.business.ViewComponenteServidor;
 
 import org.example.dao.ViewComponenteServidorDAO;
-
-import org.example.looca.CpuLooca;
-import org.example.looca.MemoriaLooca;
-import org.example.looca.DiscoLooca;
-import org.example.looca.RedeLooca;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,11 +14,11 @@ public class Main {
         String macAddress = "09:11:44:1F:3A:A9";
 
         List<ViewComponenteServidor> listaComponentesServidor = ViewComponenteServidorDAO.consultarComponenteMedida(macAddress);
-        List<ComponenteMedida> listaComponentesMedida = new ArrayList<>();
+        List<ComponenteMedidaComando> listaComponentesMedida = new ArrayList<>();
 
         listaComponentesServidor.forEach(item -> {
             System.out.println(item);
-            listaComponentesMedida.add(new ComponenteMedida(
+            listaComponentesMedida.add(new ComponenteMedidaComando(
                     item.getComponente(), item.getMedida(), item.getComandoJava()
             ));
         });
--- LeituraComponentesAutomatizada/src/main/java/org/example/business/ComponenteMedida.java
+++ LeituraComponentesAutomatizada/src/main/java/org/example/business/ComponenteMedida.java
@@ -1,42 +1,5 @@
 package org.example.business;
 
 public class ComponenteMedida {
-    private String componente;
-    private String medida;
-    private String comando;
-
-    public ComponenteMedida(String componente, String medida, String comando) {
-        this.componente = componente;
-        this.medida = medida;
-        this.comando = comando;
-    }
-
-    public String getComponente() {
-        return componente;
-    }
-
-    public void setComponente(String componente) {
-        this.componente = componente;
-    }
-
-    public String getMedida() {
-        return medida;
-    }
-
-    public void setMedida(String medida) {
-        this.medida = medida;
-    }
-
-    public String getComando() {
-        return comando;
-    }
-
-    public void setComando(String comando) {
-        this.comando = comando;
-    }
-
-    @Override
-    public String toString() {
-        return "[%s,%s,%s]".formatted(getComponente(), getMedida(), getComando());
-    }
+    private String nome_componente_medida;
 }
